d1bb36780a1b8189c6fdacfc2673fd0f06899189
Julian Martinez <julian.martinez.s@outlook.com>
2022-05-30 15:39:40 -0400

    Initial Commit

---
+++ FormattedText/FormattedTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormattedTextApp.swift
+//  FormattedText
+//
+//  Created by Julian Martinez on 5/30/22.
+//
+
+import SwiftUI
+
+@main
+struct FormattedTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
