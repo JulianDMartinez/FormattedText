15b8bb503876bbb4d7fa584c8e0189225cbc37a2
Julian Martinez <julian.martinez.s@outlook.com>
2022-05-30 16:28:52 -0400

    Tutorial updates.

--- FormattedText/ContentView.swift
+++ FormattedText/ContentView.swift
@@ -12,30 +12,39 @@ struct ContentView: View {
     @State var someText = "initial text"
     
     var body: some View {
-        VStack {
-            SecureField("Enter Password", text: $password)
-                .padding()
-            Text("Password Entered: \(password)")
-                .italic()
-            TextField("Enter Some Text", text: $someText)
-                .padding()
-            Text("\(someText)")
-                .font(.largeTitle)
-                .underline()
-            Text("Changing text color and makr it bold")
-                .foregroundColor(Color.blue)
-                .bold()
-            Text("Use kerning to change space between lines of text")
-                .kerning(7)
-            Text("Changing baseline offset")
-                .baselineOffset(100)
-            Text("Stringthrough")
-                .strikethrough()
-            Text("This is a multiline text implemented in SwiftUI. The trailing modifier was added to the text. This text also implements multiple modifiers")
-                .background(Color.yellow)
-                .multilineTextAlignment(.trailing)
-                .lineSpacing(10)
+        Group{
+            VStack {
+                Group {
+                    Text("Hellow World")
+                        .padding()
+                    SecureField("Enter Password", text: $password)
+                        .padding()
+                    Text("Password Entered: \(password)")
+                        .italic()
+                    TextField("Enter Some Text", text: $someText)
+                        .padding()
+                    Text("\(someText)")
+                        .font(.largeTitle)
+                        .underline()
+                    Text("Changing text color and makr it bold")
+                        .foregroundColor(Color.blue)
+                        .bold()
+                    Text("Use kerning to change space between lines of text")
+                        .kerning(7)
+                    Text("Changing baseline offset")
+                        .baselineOffset(100)
+                    Text("Stringthrough")
+                        .strikethrough()
+                    Text("This is a multiline text implemented in SwiftUI. The trailing modifier was added to the text. This text also implements multiple modifiers")
+                        .background(Color.yellow)
+                        .multilineTextAlignment(.trailing)
+                        .lineSpacing(10)
+                }
+                Text("11 Element")
+                    .padding()
+            }
         }
+
     }
 }
 
